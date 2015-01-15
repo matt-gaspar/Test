242e068c4615a3d2f9f4fe5d16b72da7990e0b09
matt-gaspar <matt.gaspar@digital.landregistry.gov.uk>
2015-01-15 13:57:47 +0000

    Done some work here

--- Flask-App/flask-app.py
+++ Flask-App/flask-app.py
@@ -3,5 +3,10 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello():
 	return 'Hello The Real World!'
+	
+@app.route('/goodbye'):
+def goodbye():
+	return 'See ya . . .'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
